80d7c8f77f2c9d23d635c8a030f284add8d14090
AhzamRB <ahzambari@gmail.com>
2024-06-30 18:11:34 +0500

    Profile page is still not done, need to finalize page being scrollable instead of container, and make edit page

--- lib/pages/app_pages/profile.dart
+++ lib/pages/app_pages/profile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:supabase_auth/utilities/dimensions.dart';
-import 'package:supabase_auth/utilities/profile_page_richtext.dart';
+import 'package:supabase_auth/utilities/profile_info_container.dart';
+import 'package:supabase_auth/utilities/profile_picture_container.dart';
+import 'package:supabase_auth/utilities/buttons.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -9,56 +9,19 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButton: Padding(
+            padding: const EdgeInsets.only(bottom: 60.0),
+            child: SizedBox(
+              height: 35,
+              width: 100,
+              child: CustomButtons.editButton(onPressed: () {}),
+            )),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
         backgroundColor: Colors.transparent,
         body: Center(
           child: Stack(alignment: Alignment.center, children: [
-            Container(
-                margin: EdgeInsets.only(top: Dimensions(context).height(50)),
-                width: Dimensions(context).width(85),
-                height: Dimensions(context).height(22),
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.18),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: const SingleChildScrollView(
-                  padding: EdgeInsets.fromLTRB(10.0, 40.0, 10.0, 10.0),
-                  child: ProfileRichText(),
-                )),
-            Positioned(
-              bottom: 20,
-              right: 10,
-              child: SizedBox(
-                height: 35,
-                width: 100,
-                child: FloatingActionButton.extended(
-                    icon: const Icon(
-                      Icons.edit,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {},
-                    backgroundColor: const Color(0xFFFA2A55),
-                    label: const Text('Edit'),
-                    foregroundColor: Colors.white),
-              ),
-            ),
-            Container(
-                margin: EdgeInsets.only(bottom: Dimensions(context).height(20)),
-                width: Dimensions(context).width(85),
-                height: Dimensions(context).height(55),
-                decoration: BoxDecoration(
-                    color: const Color(0xFFD9D9D9),
-                    borderRadius: BorderRadius.circular(15),
-                    border:
-                        Border.all(color: const Color(0xFFFA2A55), width: 1.5),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.5),
-                        spreadRadius: 2,
-                        blurRadius: 5,
-                        offset: const Offset(0, 3),
-                      )
-                    ]),
-                child: Image.asset('./assets/images/muneeb1.png'), ),
+            ProfileInfoContainer(),
+            ProfileImageContainer(),
           ]),
         ));
   }
